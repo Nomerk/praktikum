4ca1e8a5cbec512f9381c80f2e126efc396bdd96
Nomerk <97950056+Nomerk@users.noreply.github.com>
2023-09-22 14:30:33 +0700

    Create praktikum.py

---
+++ praktikum.py
@@ -0,0 +1,7 @@
+panjang_ruangan = input("Masukkan Panjang Ruangan: ")
+lebar_ruangan = input("Masukkan Lebar Ruangan: ")
+satuan = input("Masukkan Satuan (Meter/Inci): ")
+
+luas_ruangan = float(panjang_ruangan) * float(lebar_ruangan)
+
+print("Luas ruangan dengan panjang", panjang_ruangan, "dan lebar", lebar_ruangan, "adalah", luas_ruangan, satuan)
